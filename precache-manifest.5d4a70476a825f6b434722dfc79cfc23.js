50396afdca70da5596dad13ed43b0e7fabc12f08
Nicolo <54029524+grantortino@users.noreply.github.com>
2020-06-04 13:48:13 +0200

    Updates

---
+++ precache-manifest.5d4a70476a825f6b434722dfc79cfc23.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89116555768c10c317eff41e6edeadbb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0df30d3e794228df00a8",
+    "url": "./static/css/main.8544dc23.chunk.css"
+  },
+  {
+    "revision": "8893aaa5c0de5d1d09b9",
+    "url": "./static/js/2.dbc1c377.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.dbc1c377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0df30d3e794228df00a8",
+    "url": "./static/js/main.2c165f03.chunk.js"
+  },
+  {
+    "revision": "7161d27d43437fd7a253",
+    "url": "./static/js/runtime-main.0ae6a310.js"
+  },
+  {
+    "revision": "94beaea46c56ffd9be1defcf2a19246b",
+    "url": "./static/media/bass.94beaea4.jpg"
+  },
+  {
+    "revision": "c0205497ae8eec9b5a30a3b2f0ef9d57",
+    "url": "./static/media/drums.c0205497.jpg"
+  },
+  {
+    "revision": "f1d35889f2c7c7c2809a5ea0464e857a",
+    "url": "./static/media/guitar.f1d35889.jpg"
+  },
+  {
+    "revision": "12f9f2835b2569e4313d0acae145f193",
+    "url": "./static/media/tool-logo.12f9f283.png"
+  },
+  {
+    "revision": "9c0a13aaf54ce0032a572697a8bfa533",
+    "url": "./static/media/vocals.9c0a13aa.jpg"
+  }
+]);
